ad16092234ec279ef17955763ed7b2dbdbe8a2b4
Dmitry Brusenin <dbrusenin@dbrusenin-vm.sas.yp-c.yandex.net>
2024-06-13 20:22:40 +0300

    add sql attribute

--- programs/query-parser/ASTSerialization.cpp
+++ programs/query-parser/ASTSerialization.cpp
@@ -1,11 +1,14 @@
 #include "ASTSerialization.hpp"
 
+#include <Parsers/formatAST.h>
+
 #include "ASTCustomAttributes.hpp"
 
 
 ASTNodeBuilder MakeBuilder(DB::ASTPtr ast) {
     ASTNodeBuilder builder("n" + std::to_string(reinterpret_cast<std::uintptr_t>(ast.get())));
     builder.emplaceAttribute("label", SerializeToJSON(ast->getID(' ')));
+    builder.emplaceAttribute("sql", SerializeToJSON(DB::serializeAST(*ast)));
     for (const auto& attr: GetCustomAttributes(ast.get())) {
         builder.addAttribute(attr);
     }
